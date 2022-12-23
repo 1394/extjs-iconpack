abb34ebf7a1902aba4c6278489a05f6f1cc48d9d
Перфильев Сергей Эдуардович <sperfilev@msc.sigma-it.local>
2022-12-23 19:32:03 +0300

    Create: icon package (move from packages\local)

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+    'root': true,
+    'env': {
+        'browser': true,
+        'es6': true,
+        'node': true,
+    },
+    'extends': [
+        'google'
+    ],
+    'globals': {
+        'Ext': 'readonly',
+        'JSON5': 'readonly',
+        'Coon': 'writable',
+    },
+    'parserOptions': {
+        ecmaVersion: 8,
+    },
+    'rules': {
+        'max-len': [
+            'error',
+            150
+        ],
+        'indent': [
+            'error',
+            4
+        ],
+
+        'prefer-const': 'error',
+        'no-const-assign': 'error',
+        'guard-for-in': ['off'],
+        'valid-jsdoc': ['off'],
+        'prefer-rest-params': ['off'],
+        'prefer-spread': ['off'],
+        'new-cap': ['off', {'newIsCap': false}],
+        'semi': ['error', 'always'],
+        'curly': ['error', 'all'],
+        'comma-dangle': ['error', {
+            'arrays': 'never',
+            'objects': 'always-multiline',
+            'imports': 'never',
+            'exports': 'never',
+            'functions': 'never',
+        }],
+        'camelcase': 0,
+        'no-undef': ['error'],
+    },
+};
